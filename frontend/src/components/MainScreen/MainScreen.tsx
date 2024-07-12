13fb260496a8c86d855e6133c34be4aea194bda6
volxdya <recommendation082@gmail.com>
2024-07-12 22:33:04 +0500

    some added to circle

--- frontend/src/components/MainScreen/MainScreen.tsx
+++ frontend/src/components/MainScreen/MainScreen.tsx
@@ -1,22 +1,25 @@
 import './MainScreen.scss';
-import {Play} from "../../icons/Play.tsx";
-import {MainCard} from "../../ui/MainCard/MainCard.tsx";
+import { Play } from "../../icons/Play.tsx";
+import { MainCard } from "../../ui/MainCard/MainCard.tsx";
 
 export function MainScreen() {
     return (
         <>
             <div className="d-flex justify-content-center circle-container">
-                <div className="circle d-flex justify-content-center"/>
+                <div className="circle d-flex justify-content-center">
+                    <div className='c-2'></div>
+                    <div className='c-3'></div>
+                </div>
             </div>
             <button className="title-wave">
                 <div className="d-flex align-items-center gap-2 wave-button">
-                    <Play/>
+                    <Play />
                     Моя волна
                 </div>
             </button>
             <div className="container-playlists d-flex gap-3">
-                <MainCard/>
-                <MainCard/>
+                <MainCard />
+                <MainCard />
             </div>
         </>
 
