d89612b37bcd4a3d2ab7afee69d7f3a57c2d00b1
volxdya <recommendation082@gmail.com>
2024-07-17 12:20:40 +0500

    added controls on player + fixed scss scripts

--- frontend/src/components/Player/Player.tsx
+++ frontend/src/components/Player/Player.tsx
@@ -2,31 +2,38 @@ import './Player.scss';
 import {Play} from "../../icons/Player/Play.tsx";
 import {Random} from "../../icons/Player/Random.tsx";
 import {Repeat} from "../../icons/Player/Repeat.tsx";
+import {Dots} from "../../icons/Dots.tsx";
+import {SecondHeart} from "../../icons/Hearts/SecondHeart.tsx";
 
 export function Player() {
     return (
         <div className="player d-flex justify-content-between align-items-center">
-            <div className="d-flex gap-3">
+            <div className="d-flex gap-3 track-data-player">
                 <img
                     src="https://i1.sndcdn.com/artworks-ECKXdM5RsSBxdXja-acLzLg-t500x500.jpg"
-                     alt="Картинка трека"
+                    alt="Картинка трека"
                     className="player-img"
                 />
-                <div className="d-flex align-items-center">
+                <div className="d-flex align-items-center gap-5">
                     <div>
                         <p className="m-0 player-title">Lo siento</p>
                         <p className="m-0 player-author">Big baby tape</p>
                     </div>
+
+                    <div className="controls-player d-flex gap-4">
+                        <SecondHeart/>
+                        <Dots/>
+                    </div>
                 </div>
             </div>
             <div>
-                <div className="d-flex gap-5 justify-content-center align-items-center controls-track-container">
+                <div className="d-flex gap-5 mt-2 justify-content-center align-items-center controls-track-container">
                     <Random/>
                     <Play/>
                     <Repeat/>
                 </div>
 
-                <div className="d-flex align-items-center gap-3 mt-2">
+                <div className="d-flex align-items-center gap-3 mt-2 player-data">
                     <p className="m-0 time-player">00:17</p>
                     <div className="progress-bar-container">
                         <div className="progress-bar-player" style={{width: `12%`}}/>
@@ -34,7 +41,7 @@ export function Player() {
                     <p className="m-0 time-player">03:14</p>
                 </div>
             </div>
-            <div>Settings</div>
+            <div className="settings-player d-flex justify-content-end">Settings</div>
         </div>
     );
 }
