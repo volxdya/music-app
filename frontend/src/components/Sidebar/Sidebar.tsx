0adba6bedd10e18de460b93615fa917a8feab410
volxdya <recommendation082@gmail.com>
2024-07-22 13:51:21 +0500

    added logic

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -3,14 +3,13 @@ import {Search} from "../../icons/Search.tsx";
 import {Note} from "../../icons/Note.tsx";
 import {Heart} from "../../icons/Hearts/Heart.tsx";
 import {Link, useLocation} from "react-router-dom";
-import {Dropdown} from "../Dropdown/Dropdown.tsx";
 import {
     DropdownMenu,
-    DropdownMenuContent, DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
+    DropdownMenuContent,
     DropdownMenuTrigger
 } from "@radix-ui/react-dropdown-menu";
+import {Settings} from "@/icons/Settings.tsx";
+import {LogOut} from "@/icons/LogOut.tsx";
 
 interface ISidebar {
     component: React.ReactNode;
@@ -58,32 +57,55 @@ export function Sidebar() {
             <div className="me">
                 <div className="d-flex gap-3">
 
+                    <div>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <div>
+                                    <img
+                                        src="https://i.pinimg.com/736x/45/4e/e6/454ee61e8e8ee73efa8623c2688d22d2.jpg"
+                                        alt="Аватарка пользователя"
+                                        className="avatar"
+                                    />
+                                </div>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <div className="test mx-4 p-3">
+                                    <div className="d-flex justify-content-center mt-2">
+                                        <img
+                                            src="https://i.pinimg.com/736x/45/4e/e6/454ee61e8e8ee73efa8623c2688d22d2.jpg"
+                                            alt="Аватарка пользователя"
+                                            className="avatar"
+                                        />
+                                    </div>
+                                    <div className="mt-2">
+                                        <p className="text-center">apathy</p>
+                                        <p className="text-center name-user">Владимир Тестов</p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <div className="mt-1 menu-item">
+                                            <div className="d-flex align-items-center gap-2">
+                                                <Settings/>
+                                                Настройки
+                                            </div>
+                                        </div>
+                                        <div className="mt-1 menu-item">
+                                            <div className="d-flex align-items-center gap-2">
+                                                <LogOut/>
+                                                Выйти
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
 
-
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <div>
-                                <img
-                                    src="https://i.pinimg.com/736x/45/4e/e6/454ee61e8e8ee73efa8623c2688d22d2.jpg"
-                                    alt="Аватарка пользователя"
-                                    className="avatar"
-                                />
-                            </div>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator/>
-                            <DropdownMenuItem>Profile</DropdownMenuItem>
-                            <DropdownMenuItem>Billing</DropdownMenuItem>
-                            <DropdownMenuItem>Team</DropdownMenuItem>
-                            <DropdownMenuItem>Subscription</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
                     <div>
                         <p>apathy</p>
                     </div>
                 </div>
             </div>
         </nav>
-    );
+    )
+        ;
 }
