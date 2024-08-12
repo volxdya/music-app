d678d1e84bc82ec114e9cca7c08251fda99abbb9
volxdya <recommendation082@gmail.com>
2024-08-12 16:45:00 +0500

    addded genre cards

--- frontend/src/ui/Cards/GenreCard/GenreCard.tsx
+++ frontend/src/ui/Cards/GenreCard/GenreCard.tsx
@@ -1,9 +1,17 @@
-import './GenreCard.scss';
+import "./GenreCard.scss";
+import { Play } from "@/icons/Play.tsx";
 
 interface Props {
   title: string;
 }
 
 export function GenreCard({ title }: Props) {
-  return <div className="genre-card mt-3">{title}</div>;
+  return (
+    <div className="genre-card mt-3">
+      <div className="genre-card-wrapper d-flex align-items-center">
+          <Play />
+          <p>{title}</p>
+      </div>
+    </div>
+  );
 }
