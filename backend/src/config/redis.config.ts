a73846e022dc27072d59e1ef81985d5e7a136f7b
Vladimir <recommendation082@gmail.com>
2024-08-31 14:16:23 +0500

    added logics to docker-compose, but not full =DDD

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -1,9 +1,9 @@
-import { redisStore } from "cache-manager-redis-yet";
+import { redisStore } from 'cache-manager-redis-yet';
 
 export const REDIS_CONFIG = {
-    store: redisStore,
-    isGlobal: true,
-    host: process.env.REDIS_HOST,
-    ttl: 1000 * 60,
-    port: process.env.REDIS_PORT,
-}
+  store: redisStore,
+  isGlobal: true,
+  host: process.env.REDIS_HOST,
+  ttl: 1000 * 60,
+  port: process.env.REDIS_PORT,
+};
