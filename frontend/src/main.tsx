1f1a9b706ce8575ebd813bb3b7e817f482f2ec69
volxdya <recommendation082@gmail.com>
2024-09-08 01:14:57 +0500

    deleted trash dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,21 +4,12 @@ import './global.scss'
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster.tsx";
 import App from "@/App.tsx";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query';
-
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>,
 )
